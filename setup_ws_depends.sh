ef10d46d26268143e72cbdf5ff30675853c89499
ipa-nhg <nhg@ipa.fhg.de>
2019-04-17 09:07:19 +0200

    test script

--- setup_ws_depends.sh
+++ setup_ws_depends.sh
@@ -41,8 +41,9 @@ cd ..
 touch src/cob_hand/cob_hand_bridge/CATKIN_IGNORE
 touch src/rplidar_ros/CATKIN_IGNORE
 touch src/cob_control/cob_obstacle_distance/CATKIN_IGNORE
+touch src/moveit/moveit_kinematics/CATKIN_IGNORE
 #catkin_make_isolated -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_CXX_COMPILER=/usr/bin/clang++-3.8 -DCATKIN_BLACKLIST_PACKAGES="$pkg_blacklist"
-catkin_make_isolated --only-pkg-with-deps $1 -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_CXX_COMPILER=/usr/bin/clang++-3.8 --merge
+catkin_make_isolated --only-pkg-with-deps $1 -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_CXX_COMPILER=/usr/bin/clang++-3.8 --merge #(--force-cmake)
 
 mkdir build && touch build/compile_commands.json
 cat build_isolated/*/compile_commands.json >> build/compile_commands.json
@@ -50,4 +51,5 @@ sed -i -e 's/\]\[/\,/g' build/compile_commands.json
 
 source devel_isolated/setup.bash
 
-roslaunch-dump src/cob_robots/cob_bringup/robots/cob4-10.launch robot:=cob4-10 robot_env:=empty > src/cob_robots/cob_bringup/robots/cob4_ros_tool_test.launch
+roslaunch-dump src/cob_robots/cob_bringup/robots/cob4-10.launch robot:=cob4-10 robot_env:=empty > src/cob_robots/cob_bringup/robots/ 
+ros_model_extractor.py --package cob_bringup --name cob4_ros_tool_test.launch --launch --output true
